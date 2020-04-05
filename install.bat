a2c203d924134c6298bcc2511e5024e89e3b17a8
yrong <yrong1997@gmail.com>
2020-04-05 21:33:25 +0800

    release fix

--- install.bat
+++ install.bat
@@ -1,1 +1,3 @@
+cd /d %~dp0
 powershell -executionpolicy unrestricted -File init.ps1
+cmd /k
--- release.sh
+++ release.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-zip -r release.zip . -x "*/\.*" -x ".env" -x "*.idea*" -x "*.git*" -x "*.DS_Store" -x "*node_modules*" -x "*download*"
+rm -rf release.zip
+zip -r release.zip . -x "*/\.*" -x ".env" -x "*.idea*" -x "*.git*" -x "*.DS_Store" \
+-x node_modules/\* -x download/\* -x test/\* -x "*.log"
