a2ca23f7267f983f5d1280b680f34d81c982b78a
yrong <yrong1997@gmail.com>
2020-04-16 11:20:07 +0800

    windows release

---
+++ release.ps1
@@ -0,0 +1,15 @@
+webpack
+showdown makehtml -i README.md -o README.html --tables
+
+$zipName="release.zip"
+if (Test-Path $zipName) 
+{
+  Remove-Item $zipName
+}
+
+$compress = @{
+  Path = "dist","public",".env.example","package.json","package-lock.json","install.bat","install.ps1", "ecosystem.config.js","README.html","appreciate.jpg","analysis.png","detail.png"
+  CompressionLevel = "Fastest"
+  DestinationPath = $zipName
+}
+Compress-Archive @compress
