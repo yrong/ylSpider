f3393ce30fdc5f8285d4ce94ede313c78dbe672e
yrong <yrong1997@gmail.com>
2020-05-11 19:14:31 +0800

    release

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf dist
 webpack
 
 readme="yooli.html"
@@ -7,7 +8,7 @@ showdown makehtml -u UTF8 --tables -i README.md -o $readme
 sed -i '1s/^/<meta charset="UTF-8">/' $readme
 
 rm -rf release.zip
-zip -r release.zip \r
+zip -r release.zip \
 dist public .env.example package.json package-lock.json ecosystem.config.js \
 install.bat install.ps1 start.bat start.ps1 \
 appreciate.jpg analysis.png detail.png $readme
