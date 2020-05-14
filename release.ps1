6f2cb1b8627ada4cb7b5233bc8e64579d8a7d390
yrong <yrong1997@gmail.com>
2020-05-14 13:31:09 +0800

    fix ps

--- release.ps1
+++ release.ps1
@@ -10,9 +10,24 @@ if (Test-Path $zipName)
   Remove-Item $zipName
 }
 
+if (Test-Path .env) 
+{
+	move-item -path .env -destination .envbak -Force
+}
+
+copy-item -path .env.example -destination .env
+
+
 $compress = @{
-  Path = "dist","public",".env.example","package.json","package-lock.json","install.bat","install.ps1","start.bat","start.ps1","ecosystem.config.js",$readme,"appreciate.jpg","analysis.png","detail.png"
+  Path = "dist","public",".env.example",".env","package.json","package-lock.json","install.bat","install.ps1","start.bat","start.ps1","ecosystem.config.js",$readme,"appreciate.jpg","analysis.png","detail.png"
   CompressionLevel = "Fastest"
   DestinationPath = $zipName
 }
 Compress-Archive @compress
+
+if (Test-Path .envbak) 
+{
+	move-item -path .envbak -destination .env -Force
+}
+
+
